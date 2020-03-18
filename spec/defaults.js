2f8d5a514d99604ea69d68e121b3edd80c2f3753
Daniel Getz <tanketz@gmail.com>
2020-03-18 03:05:33 -0400

    add COMPRAS_HEADLESS=no env var option to tests

--- spec/defaults.js
+++ spec/defaults.js
@@ -4,9 +4,13 @@ const {Builder} = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 
 function createBrowser() {
+    let opts = new chrome.Options();
+    if (process.env.COMPRAS_HEADLESS !== 'no') {
+        opts = opts.headless();
+    }
     return new Builder()
         .forBrowser('chrome')
-        .setChromeOptions(new chrome.Options().headless())
+        .setChromeOptions(opts)
         .build();
 }
 
